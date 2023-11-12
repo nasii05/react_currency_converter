b0e2f58e3ce5c7857c70236dd704ad05328a5b7d
Nasih <iamnasih@gmail.com>
2023-11-12 17:34:46 +0530

    input box setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='w-full h-screen flex flex-wrap
+    justify-center items-center bg-cover bg-no-repeat
+    bg-gradient-to-tr from-blue-200 via-blue-300 to-blue-400'>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default App;
